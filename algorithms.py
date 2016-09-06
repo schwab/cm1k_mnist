a59d30d037c3597a39e716956bcdc504d90d2c15
Michael Schwab <mcstar@gmail.com>
2016-09-06 22:32:36 +0000

    compress algorithm

--- algorithms.py
+++ algorithms.py
@@ -1,18 +1,88 @@
-def generateTemplateData():
-    templates =[]
-    last = 1
-    for i in range(0,218):
-        mask = int("{0:b}".format(i))
-        mask = mask & 0b0000000011111111
-        t = [0,0,0,0,0,0,0,0]
-        print i,mask
-        for m in range(0,8):
-            #print last & mask
-            if last & mask:
-                t[m] = 255
-            mask = mask>>1
-        print t
-        templates.append(t)
+import json
+import random
 
-    return templates
+### templates for generating training data for cmk1k
+class compression_templates:
 
+    ### Returns a training set for the CM1K allowing it to perform lossy compression of 8 bytes of data to just one
+    ### This is useful for converting grey scale images where the only infomration needed is whether a pixel is off or on
+    ### for instance, on this pattern loaded on the CM1K will convert [255,255,255,255,0,0,0,0] to 11110000 
+    def eightToOneByteCompressionAlgorithm(self):
+        templates =[]
+        last = 1
+        for i in range(0,256):
+            mask = int("{0:b}".format(i))
+            #data = mask & 0b0000001111111111
+            data = i
+            t = [0,0,0,0,0,0,0,0]
+            t2 = [0,0,0,0,0,0,0,0]
+            #print i,mask, data
+            for m in range(0,8):
+                #print last & mask
+                if last & data:
+                    t[m] =127 
+                    t2[m] = 255
+                data = data>>1
+            trainData = {"x":t,"y":i}
+            templates.append(trainData)
+            trainData = {"x":t2,"y":i}
+            templates.append(trainData)
+
+        return templates
+    def save8to1TrainingSet(self):
+
+        t = self.eightToOneByteCompressionAlgorithm()
+        lst =[]
+        with open("downsample_keyframes.json",'w') as  f:
+            json.dump(t,f)
+
+    def compressionTestData(self,count):
+        testRange = range(0,count)
+        testSet=[]
+        random.seed()
+        for t in testRange:
+            testBin = 0
+            vector =[0,0,0,0,0,0,0,0]
+            for b in range(0,8):
+                vItem = random.randint(1,10)
+                if vItem > 4:
+                    testBin = testBin | 1
+                    vItem = 255
+                else:
+                    vItem=0
+                testBin = testBin << 1
+                vector[b]=vItem
+            testBin = testBin >> 1
+            testSet.append({"x":vector,'y':testBin,'bStr':"{0:b}".format(testBin)})
+        #print testSet
+        return testSet
+    def testCompression(self,input,bl):
+        perfectMatches=0
+        totalError=0
+        totalTests=0
+        for item in input:
+            respLevel, result = bl.recVec(item['x'])
+            #print 'Expected Cat: ',item['y'],'Actual Cat:',result[0][1],'Dist:',result[0][0]
+            if item['y'] == result[0][1]:
+                perfectMatches+=1
+            else:
+                totalError+=abs(item['y'] - result[0][1])
+
+            totalTests+=1
+        print  "Tests",totalTests,"Matches",perfectMatches,"TotalError",totalError
+    #def testCompressCpuImage(self,img):
+    
+    def compressCpu(self,indata):
+        data=0
+        #print indata
+        for x in range(0,7):
+            if indata[x] > 20:
+                data = data + 1
+                #print data
+            data = data << 1
+        return data
+    def compressCpuBatch(self,inset):
+        data = []
+        for item in inset:
+            data.append(self.compressCpu(item['x']))
+        return data
--- mnist_1.py
+++ mnist_1.py
@@ -1,113 +1,161 @@
 ##
-## Simple Script to test access to the neurons of the BrainCard
+## Train and test the BrainCard on the MNIST data card
 ##
-## Refer to Docs/Test_SimpleScript.pdf from General Vision for
-## details about this script.
 ##
 import time
 import Braincard
 import json
 from constants import *
 from loader import MNIST
+from algorithms import compression_templates
+
+# Perform BrainCard training and testing on the MNIST image set
 class brain_learn:
-	Braincard.connect(500000);
-	inputlen=750;
-	trvec=None	
-	train_labels =None
-	ncount=0
-	def loadMnist(self):
+    Braincard.connect(500000);
+    inputlen=750;
+    trvec=None	
+    trvecRed=None
+    train_labels =None
+    ncount=0
+    ca=None
+    minif=2
+    
+    ### Load the raw MNIST training data into training arrays	
+    def loadMnist(self):
 		m = MNIST('./data')
 		self.trvec,self.train_labels = m.load_training()
-	def initAll(self):
-		
-		Braincard.write(CM1K,FORGET,0);
-		time.sleep(0.1)
-	def learnItem(self,id):
-		self.learn(self.trvec[id],self.train_labels[id])
-	def learn(self,img,lab):
+	
+    ### Clear the BrainCard of any existing memories
+    def initAll(self):
+        Braincard.write(CM1K,FORGET,0)
+        time.sleep(0.1)
+        if self.minif != 2:
+            Braincard.write(CM1K,MINIF,self.minif)
+            print "Set MINIF  to", self.minif
+    
+    ### Train the Braincard on a single row from the training data by index
+    def learnItem(self,id):
+		self.learn(self.trvecRed[id],self.train_labels[id])
+        ### Learn a vector and categorize it by label
+
+    # Learn a single image given its label
+    def learn(self,img,lab):
 		self.ncount = Braincard.learn(img,lab)
 		print "learned ",lab, "committed:", self.ncount
-	def learnSet(self,s,e):
+    
+    # learn a set of items from the training set 
+    def learnSet(self,s,e):
 		for x in range(s,e):
-			self.learn(self.trvec[x],self.train_labels[x])
-	def recVec(self,v):
-		respLevel, results=Braincard.recognize(v,5);
-		print respLevel,results
-		if respLevel > 0:
-			for i in range(0,respLevel):
-		    		print ("nid=%u \tcat=%u \tdist=%u" % (results[i][2], results[i][1], results[i][0]));
-	def learnAdding(self):
-		self.trvec=[[1,255,0,128,0],[255,1,128,10],[255,255,255,0],[255,255,0,0],[0,0,255,255],[128,1,0,128],[128,128,255,1],[1,1,1,1],[255,128,0,255],[50,45]]	
-		self.train_labels= [sum(self.trvec[0]),sum(self.trvec[1]),sum(self.trvec[2]),sum(self.trvec[3]),sum(self.trvec[4]),sum(self.trvec[5]),sum(self.trvec[6]),sum(self.trvec[7]),sum(self.trvec[8]),sum(self.trvec[9])]  	
-		for x in range(0,len(self.trvec)):
-			self.learnItem(x)
-	def recItem(self,x):
-		respLevel, results=Braincard.recognize(self.trvec[x],5);
-		print respLevel,results
-		if respLevel > 0:
-			for i in range(0,respLevel):
-		    		print ("nid=%u \tcat=%u \tdist=%u" % (results[i][2], results[i][1], results[i][0]));
-		print("Should be : ",self.train_labels[x]);
-	def showNeurons(self):
-		## display the content of the neurons
-		print("\n\nRead the content of the %u committed neurons" % self.ncount);
-		for i in range(0,self.ncount):
-	    		ncr, model, aif, minif, cat = Braincard.reviewNeuron(i)
-    			print("\nncr=%u \tcat=%u \taif=%u \tmodel:" % (ncr, cat, aif)),
-	    		for j in range(0,len(model) -1):
-			        print("\t%u" % model[j]),        
-	def showDefault(self):
-		value=Braincard.read(CM1K,MINIF);
-		print ("Read default Minif=%u" % value);
+			self.learn(self.trvecRed[x],self.train_labels[x])
 
-		value=Braincard.read(CM1K,MAXIF);
-		print ("Read default Maxif=%u" % value);
-	def learnSample(self):
-		vector=[0 for i in range(self.inputlen)]
+    # submit the vector v to the braincard and return the regoction details
+    def recVec(self,v):
+        respLevel,results=Braincard.recognize(v,5)
+        return (respLevel,results)
+    
+    def compressAllTrvec(self):
+        ic = image_converter()
+        self.trvecRed = []
+        for img in self.trvec:
+            r =ic.downsample(img)
+            self.trvecRed.append(r)
+            print "converted ", len(self.trvecRed), 'images'
+    
+    def recognizeReport(self,idx):
+        r1,r2=self.recVec(self.trvecRed[idx])
+        print "expected cat",self.train_labels[idx]
+        self.printResponse(r1,r2)
+    
+    def calcTrainError(self):
+        errorData={'successCount':0,'errorCount':0,'errorIds':[]}
+        for x in range(0,len(self.trvecRed)):
+            r1,r2 = self.recVec(self.trvecRed[x])
+            if r1 > 0  and r2[0][1] == self.train_labels[x] :
+                errorData['successCount'] = errorData['successCount']+1
+                print "Success", x
+            else:
+                errorData['errorCount'] = errorData['errorCount'] + 1
+                errorData['errorIds'].append(x)
+                print "Error", x
+        return errorData
 
-		print("\nLearn as category 55 the vector "),
-		for i in range(0,self.inputlen):
-		    vector[i]=11
-		    print("%u, " % (vector[i])),
-		Braincard.learn(vector,55);
-
-		print("\nLearn as category 33 the vector "),
-		for i in range(0,self.inputlen):
-		    vector[i]=15
-		    print("%u, " % (vector[i])),
-		Braincard.learn(vector,33);
-
-		print("\nLearn as category 100 the vector "),
-		for i in range(0,self.inputlen):
-		    vector[i]=20
-		    print("%u, " % (vector[i])),
-		ncount=Braincard.learn(vector,100);
-	def showContentSample(self,ncount,img):
-		## display the content of the neurons
-		print("\n\nRead the content of the %u committed neurons" % ncount);
-		for i in range(0,ncount):
-		    ncr, model, aif, minif, cat = Braincard.reviewNeuron(i)
-		    print("\nncr=%u \tcat=%u \taif=%u \tmodel:" % (ncr, cat, aif)),
-		    for j in range(0,10):
-			print("\t%u" % model[j]),        
-
-		## In case of multiple firing, read the response of
-		## at least the top 3 firing neurons
-		K=3;
-
-		print("\n\nBroadcast image "),
-		respLevel, results=Braincard.recognize(img,K);
+    ### teach the braincard to do sums
+    def printResponse(self,resp,results):
+	    print resp,results
+	    if resp > 0:
+	        for i in range(0,resp):
+                    print ("nid=%u \tcat=%u \tdist=%u" % (results[i][2], results[i][1], results[i][0]));
+    
+    def learnAdding(self):
+	    self.trvec=[[1,255,0,128,0],[255,1,128,10],[255,255,255,0],[255,255,0,0],[0,0,255,255],[128,1,0,128],[128,128,255,1],[1,1,1,1],[255,128,0,255],[50,45]]	
+	    self.train_labels= [sum(self.trvec[0]),sum(self.trvec[1]),sum(self.trvec[2]),sum(self.trvec[3]),sum(self.trvec[4]),sum(self.trvec[5]),sum(self.trvec[6]),sum(self.trvec[7]),sum(self.trvec[8]),sum(self.trvec[9])]  	
+	    for x in range(0,len(self.trvec)):
+	    	self.learnItem(x)
+    
+    def learnCompression(self):
+        #self.initAll()
+        c = compression_templates()
+        self.ca = c.eightToOneByteCompressionAlgorithm()
+        self.trvec =[]
+        self.train_labels=[]
+        for item in self.ca:
+            self.trvec.append(item["x"])
+            self.train_labels.append(item["y"])
+        self.learnSet(0,len(self.trvec))
+    
+    def recItem(self,x):
+	    respLevel, results=Braincard.recognize(self.trvec[x],5)
+	    print respLevel,results
+	    if respLevel > 0:
 		for i in range(0,respLevel):
-		    print ("nid=%u \tcat=%u \tdist=%u" % (results[i][2], results[i][1], results[i][0]));
-		print("Observation: recognized by neurons 1 and 2, but closer to 1\n");
+		    print ("nid=%u \tcat=%u \tdist=%u" % (results[i][2], results[i][1], results[i][0]))
+	    print("Should be : ",self.train_labels[x])
+
+    # display the contents of commit neurons
+    def showNeurons(self):
+	    ## display the content of the neurons
+	    print("\n\nRead the content of the %u committed neurons" % self.ncount)
+	    for i in range(0,self.ncount):
+	    	ncr, model, aif, minif, cat = Braincard.reviewNeuron(i)
+    		print("\nncr=%u \tcat=%u \taif=%u \tmodel:" % (ncr, cat, aif))
+	    	for j in range(0,len(model) -1):
+		    print("\t%u" % model[j]),        
+
+    # Download the current nn state from braincard and save in a file "<name>.nn"
+    def saveNetwork(self,name):
+        nn = Braincard.readNeurons(self.ncount)
+        filename=name + ".nn"
+        with open(filename,'w') as f:
+            json.dump(nn,f)
+        print "saved " ,len(nn)  ,'weigths to ',filename
+    
+    def loadNetwork(self,name):
+        #Braincard.
+        pass
+
+    def showDefault(self):
+        value=Braincard.read(CM1K,MINIF);
+        print ("Read default Minif=%u" % value);
+        value=Braincard.read(CM1K,MAXIF);
+        print ("Read default Maxif=%u" % value);
 		## save knowledge
 		## neuronsContent=Braincard.readNeurons(ncount);
+
 class image_converter:
     keyframes=[]
     def train_downsampler(self):
 
         pass
     def downsample(self,vector):
-        pass
+        ptr =0
+        reduced =[]
+        ct = compression_templates()
+        while ptr < len(vector):
+            batch = vector[ptr :ptr+ 8]
+            ptr = ptr + 8
+            coin = ct.compressCpu(batch)
+            reduced.append(coin)
+
+        return reduced
     def load_keyframes(self):
-    
+        pass    
