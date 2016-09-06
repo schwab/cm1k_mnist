6927917b29745b76aac7c9a6ae1e219c3b07f48e
Michael Schwab <mcstar@gmail.com>
2016-09-06 18:46:34 -0500

    imagevisualize

---
+++ imagevisualize/array_converter.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import random 
+from PIL import Image
+
+class converter:
+    def save_image(self,data,filename):
+        plt.imsave('image/' + filename,data.reshape(28,28),cmap=cm.gray)
+
+    def randomArray(self,alen):
+        aset = np.zeros(alen)
+        for x in range(0,alen):
+            p = random.randint(0,2)       
+            if p >= 1:
+                aset[x] = random.randint(0,255)
+        return aset
